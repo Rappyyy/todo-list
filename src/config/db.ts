503e478e90246e79ed173b14820d7aee142ba16e
unknown <academy4@talentamp.com>
2025-03-25 16:33:48 +0800

    Remove .env from Git tracking

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mySqlPool: Pool = createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'todo_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME
 });
 
 export default mySqlPool;
